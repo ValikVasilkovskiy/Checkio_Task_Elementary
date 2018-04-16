6b67825d077d6c174d3bc203f1eacf31ef39fbf1
ValikVasilkovskiy <valik.vasilkovskiy@gmail.com>
2018-04-16 17:53:03 +0300

    modify flat_list

--- Flat_list.py
+++ Flat_list.py
@@ -17,5 +17,30 @@ def flat_list(array):
                 t = ''
     return r
 
-# test
-print(flat_list([[[2]], [4, [5, 6, [-69], 6, 6, 6], 7]]))
+def deep(data):
+    stack = []
+    list(map(lambda x: stack.append(x) if '__iter__' not in dir(x) else stack.extend(deep(x)), data))
+    return stack
+
+def search_deep(data):
+    stack = []
+    for i in data:
+        if '__iter__' not in dir(i):
+            stack.append(i)
+        else:
+            stack.extend(search_deep(i))
+    return stack
+
+
+assert flat_list([[[2]], [4, [5, 6, [-69], 6, 6, 6], 7]]) == [2, 4, 5, 6, -69, 6, 6, 6, 7]
+
+assert deep([1, [1, 2], [[[1]]], -2]) == [1, 1, 2, 1, -2]
+assert deep([[1], [[-1], 2], [[[1]]], -2]) == [1, -1, 2, 1, -2]
+assert deep([1, 2, 3, 4]) == [1, 2, 3, 4]
+assert deep([1,]) == [1]
+
+
+assert search_deep([1, [1, 2], [[[1]]], -2]) == [1, 1, 2, 1, -2]
+assert search_deep([[1], [[-1], 2], [[[1]]], -2]) == [1, -1, 2, 1, -2]
+assert search_deep([1, 2, 3, 4]) == [1, 2, 3, 4]
+assert search_deep([1,]) == [1]
